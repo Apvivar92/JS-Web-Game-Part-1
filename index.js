eecadd1f7f2b9865d83da7331a845636d0e29253
Apvivar92@gmail.com <Apvivar92@gmail.com>
2022-01-31 23:44:10 -0800

    added images to .js

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+let greenCharacter = document.createElement('img');
+let pineTree = document.createElement('img');
+let shroomTree = document.createElement('img');
+let pillar = document.createElement('img');
+let crate = document.createElement('img');
+let well = document.createElement('img');
+
+function newImage(character, tree, tree2, pillars, box, waterSource) {
+  character.src = 'assets/green-character.gif';
+  character.style.position = 'fixed';
+  character.style.left = '100px';
+  character.style.bottom = '100px';
+  document.body.append(character);
+
+  tree.src = 'assets/pine-tree.png';
+  tree.style.position = 'fixed';
+  tree.style.left = '450px';
+  tree.style.bottom = '200px';
+  document.body.append(tree);
+
+  tree2.src = 'assets/tree.png';
+  tree2.style.position = 'fixed';
+  tree2.style.left = '200px';
+  tree2.style.bottom = '300px';
+  document.body.append(tree2);
+
+  pillars.src = 'assets/pillar.png';
+  pillars.style.position = 'fixed';
+  pillars.style.left = '350px';
+  pillars.style.bottom = '100px';
+  document.body.append(pillars);
+
+  box.src = 'assets/crate.png';
+  box.style.position = 'fixed';
+  box.style.left = '150px';
+  box.style.bottom = '200px';
+  document.body.append(box);
+
+  waterSource.src = 'assets/well.png';
+  waterSource.style.position = 'fixed';
+  waterSource.style.left = '500px';
+  waterSource.style.bottom = '425px';
+  document.body.append(waterSource);
+}
+
+newImage(greenCharacter, pineTree, shroomTree, pillar, crate, well);
